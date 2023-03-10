255db8ab894877dd62f387c66cba67631373c5bb
Hari123456567 <127493766+Hari123456567@users.noreply.github.com>
2023-03-10 18:15:27 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,17 @@
+a=1
+b=2
+c=a+b
+print (c)
+#DAY 4
+list1 = [7, 7, 9, 14, 40, 30, 5]
+smallest_number = min(list1)
+print(smallest_number)
+#day5
+a=int(input("enter a number"))
+b=1
+for x in range(1,a+1):
+  if x<=a:
+    b=b*x
+    x=x+1
+    print(b)
+  
